3c7f5cd51ce3fee984db5ef21c7ac9a918d95545
giovanni <giovannitarzonipiccin@gmail.com>
2025-05-14 11:54:24 -0300

    criando slide show e htmls

---
+++ src/js/script.js
@@ -0,0 +1,22 @@
+let imagens =[
+    'src/assets/casa1.jpg',
+    'src/assets/casa2.jpg',
+    'src/assets/casa3.jpg',
+]
+
+
+
+let i = 0;
+let tempo = 5000;
+
+
+
+function slideShow(){
+    document.getElementById('image').src = imagens[i];
+    i++;
+    if(i == imagens.length){
+        i=0;
+    }
+    setTimeout("slideShow()",tempo)
+}
+slideShow();
